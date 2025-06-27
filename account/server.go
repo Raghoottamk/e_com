187729d4611071d9a457fde09359a58393a65c84
Raghoottamk <raghukulkarni2004@gmail.com>
2025-06-28 02:48:31 +0530

    Completed account microservice with updated proto and cleaned old generated files

--- account/server.go
+++ account/server.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"fmt"
 	"net"
+
 	"github.com/Raghoottamk/e_com/account/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 type grpcServer struct {
+	pb.UnimplementedAccountServiceServer
 	service Service
 }
 
@@ -19,7 +21,9 @@ func ListenGRPC(s Service, port int) error {
 		return err
 	}
 	serv := grpc.NewServer()
-	pb.RegisterAccountServiceServer(serv, &grpcServer{s})
+	pb.RegisterAccountServiceServer(serv, &grpcServer{
+		UnimplementedAccountServiceServer: pb.UnimplementedAccountServiceServer{},
+		service: s,})
 	reflection.Register(serv)
 	return serv.Serve(lis)
 }
